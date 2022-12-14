b9b2881d18236a31d39b47dd8a7be60c40d3c3dd
SoudagarAzranaz <118545452+SoudagarAzranaz@users.noreply.github.com>
2022-12-14 10:37:36 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,17 @@
+function resume(){
+    let firstname;
+    firstname=document.getElementById('firstname').value;
+    console.log(firstname);
+    let middlename;
+    middlename=document.getElementById('middlename').value;
+   console.log(middlename);
+    let lastname;
+    lastname=document.getElementById('lastname').value;
+     console.log(lastname);
+    document.getElementById("name").innerHTML=firstname+middlename+lastname;}
+  
+    
+
+
+
+
